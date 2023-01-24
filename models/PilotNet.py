8b479ef0940277cf6e3bd5fe18824a99818463b4
Martin Blanckaert <57911418+Martin-blckrt@users.noreply.github.com>
2023-01-24 17:15:37 -0500

    works with TF2

---
+++ models/PilotNet.py
@@ -0,0 +1,100 @@
+import tensorflow as tf
+
+
+def _weight_variable(shape):
+    initial = tf.random.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def _bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def _conv2d(x, w, stride):
+    return tf.nn.conv2d(x, w, strides=[1, stride, stride, 1], padding='VALID')
+
+
+class PilotNet(object):
+    """An End-to-End PilotNet to output steering angles given input images of the road ahead"""
+
+    def __init__(self):
+        self.image_input = tf.keras.Input(name='selfImageInput', shape=[None, 66, 200, 3], dtype=tf.float32)
+        # use for training loss computation
+        self.y_ = tf.keras.Input(name='selfY', shape=[None, 1], dtype=tf.float32)
+        self.keep_prob = tf.keras.Input(name='selfKeepProb', shape=[None], dtype=tf.float32)
+
+        # model parameters
+        # self.model_params = []
+
+        # first convolutional layer
+        w_conv1 = _weight_variable([5, 5, 3, 24])
+        b_conv1 = _bias_variable([24])
+
+        h_conv1 = tf.nn.relu(_conv2d(self.image_input, w_conv1, 2) + b_conv1)
+
+        # second convolutional layer
+        w_conv2 = _weight_variable([5, 5, 24, 36])
+        b_conv2 = _bias_variable([36])
+
+        h_conv2 = tf.nn.relu(_conv2d(h_conv1, w_conv2, 2) + b_conv2)
+
+        # third convolutional layer
+        w_conv3 = _weight_variable([5, 5, 36, 48])
+        b_conv3 = _bias_variable([48])
+
+        h_conv3 = tf.nn.relu(_conv2d(h_conv2, w_conv3, 2) + b_conv3)
+
+        # fourth convolutional layer
+        w_conv4 = _weight_variable([3, 3, 48, 64])
+        b_conv4 = _bias_variable([64])
+
+        h_conv4 = tf.nn.relu(_conv2d(h_conv3, w_conv4, 1) + b_conv4)
+
+        # fifth convolutional layer
+        w_conv5 = _weight_variable([3, 3, 64, 64])
+        b_conv5 = _bias_variable([64])
+
+        h_conv5 = tf.nn.relu(_conv2d(h_conv4, w_conv5, 1) + b_conv5)
+
+        # FCL 1
+        w_fc1 = _weight_variable([1152, 1164])
+        b_fc1 = _bias_variable([1164])
+
+        h_conv5_flat = tf.reshape(h_conv5, [-1, 1152])
+        h_fc1 = tf.nn.relu(tf.matmul(h_conv5_flat, w_fc1) + b_fc1)
+        # h_fc1_drop = tf.nn.dropout(h_fc1, rate=self.keep_prob)
+        h_fc1_drop = h_fc1
+
+        # FCL 2
+        w_fc2 = _weight_variable([1164, 100])
+        b_fc2 = _bias_variable([100])
+
+        h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
+
+        # h_fc2_drop = tf.nn.dropout(h_fc2, rate=self.keep_prob)
+        h_fc2_drop = h_fc2
+
+        # FCL 3
+        w_fc3 = _weight_variable([100, 50])
+        b_fc3 = _bias_variable([50])
+
+        h_fc3 = tf.nn.relu(tf.matmul(h_fc2_drop, w_fc3) + b_fc3)
+
+        # h_fc3_drop = tf.nn.dropout(h_fc3, rate=self.keep_prob)
+        h_fc3_drop = h_fc3
+
+        # FCL 4
+        w_fc4 = _weight_variable([50, 10])
+        b_fc4 = _bias_variable([10])
+
+        h_fc4 = tf.nn.relu(tf.matmul(h_fc3_drop, w_fc4) + b_fc4)
+
+        # h_fc4_drop = tf.nn.dropout(h_fc4, self.keep_prob)
+        h_fc4_drop = h_fc4
+
+        # Output
+        w_fc5 = _weight_variable([10, 1])
+        b_fc5 = _bias_variable([1])
+
+        self.steering = tf.multiply(tf.atan(tf.matmul(h_fc4_drop, w_fc5) + b_fc5), 2)  # scale the atan output
---
+++ preprocess/SteeringImageDB.py
@@ -0,0 +1,65 @@
+import numpy as np
+import random
+import skimage
+
+
+class SteeringImageDB(object):
+    """Preprocess images of the road ahead and steering angles."""
+
+    def __init__(self, data_directory):
+        imgs = []
+        angles = []
+
+        # points to the end of the last batch, train & validation
+        self.train_batch_pointer = 0
+        self.val_batch_pointer = 0
+
+        # read data.txt
+        data_path = data_directory + "/"
+        with open(data_path + "data.txt") as f:
+            for line in f:
+                imgs.append(data_path + line.split()[0])
+                """ the paper by Nvidia uses the inverse of the turning radius, 
+                but steering wheel angle is proportional to the inverse of turning radius
+                so the steering wheel angle in radians is used as the output """
+                angles.append(float(line.split()[1]) * np.pi / 180)
+
+        # shuffle list of images
+        img_angle_pack = list(zip(imgs, angles))
+        random.shuffle(img_angle_pack)
+        imgs, angles = zip(*img_angle_pack)
+
+        # get number of images
+        self.num_images = len(imgs)
+
+        # split 80-20 train/validation
+        self.train_imgs = imgs[:int(self.num_images * 0.8)]
+        self.train_angles = angles[:int(self.num_images * 0.8)]
+
+        self.val_imgs = imgs[-int(self.num_images * 0.2):]
+        self.val_angles = angles[-int(self.num_images * 0.2):]
+
+        self.num_train_images = len(self.train_imgs)
+        self.num_val_images = len(self.val_imgs)
+
+    def load_train_batch(self, batch_size):
+        batch_imgs = []
+        batch_angles = []
+        for i in range(0, batch_size):
+            batch_imgs.append(skimage.transform.resize(
+                skimage.io.imread(self.train_imgs[(self.train_batch_pointer + i) % self.num_train_images])[-150:],
+                [66, 200]) / 255.0)
+            batch_angles.append([self.train_angles[(self.train_batch_pointer + i) % self.num_train_images]])
+        self.train_batch_pointer += batch_size
+        return batch_imgs, batch_angles
+
+    def load_val_batch(self, batch_size):
+        batch_imgs = []
+        batch_angles = []
+        for i in range(0, batch_size):
+            batch_imgs.append(skimage.transform.resize(
+                skimage.io.imread(self.val_imgs[(self.val_batch_pointer + i) % self.num_val_images])[-150:],
+                [66, 200]) / 255.0)
+            batch_angles.append([self.val_angles[(self.val_batch_pointer + i) % self.num_val_images]])
+        self.val_batch_pointer += batch_size
+        return batch_imgs, batch_angles
